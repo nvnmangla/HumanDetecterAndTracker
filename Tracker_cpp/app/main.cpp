9f24d0298ca5a67e80ca0cf4e719767c8c143613
Naveen Mangla <nmangla@umd.edu>
2022-10-14 17:17:17 -0400

    Added image class

---
+++ Tracker_cpp/app/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Naveen Mangla (nmangla@umd.edu)
+ * @brief Defining Functions and implementation
+ * @version 0.1
+ * @date 2022-09-20
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+#include <image.hpp>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+int main() {
+
+  return 0;
+}
---
+++ Tracker_cpp/readme.md
@@ -0,0 +1,13 @@
+ 
+Run the following commands when inside the build folder.
+
+```
+mkdir build 
+cd build 
+cmake ..
+make
+Run program: ./app/shell-app
+
+```
+This will output the program in main.cpp on the terminal.
+
