f685b57b9633aca1999f37669624992035420b7b
Naveen Mangla <nmangla@umd.edu>
2022-10-14 15:08:02 -0400

    adding cpp-boilerplate

---
+++ cpp-boilerplate/app/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <lib.hpp>
+
+int main()
+{
+    dummy();
+    return 0;
+}
+
---
+++ cpp-boilerplate/include/lib.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include<iostream>
+
+void dummy()
+{
+    std::cout << "We, cannot expect a better TA for 808X than the current one.:)" << std::endl;
+}
