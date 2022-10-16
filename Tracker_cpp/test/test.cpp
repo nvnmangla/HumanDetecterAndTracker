012d00d1e9fd9163c196b5b069641d43139bb9a3
Naveen Mangla <nmangla@umd.edu>
2022-10-16 18:48:26 -0400

    Test cases for image class

--- Tracker_cpp/test/test.cpp
+++ Tracker_cpp/test/test.cpp
@@ -1,5 +1,28 @@
-#include <gtest/gtest.h>
+/**
+ * @file test.cpp
+ * @author Naveen Mangla (nmangla@umd.edu)
+ * @brief Test cases for functions
+ * @version 0.1
+ * @date 2022-10-16
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include <gtest/gtest.h>
+#include <image.hpp>
+#include <humanDetector.hpp>
+#include <yolo.hpp>
+
+
+
+TEST(Image, enlarge) {
+  Image img;
+  EXPECT_EQ(240, img.enlarge().rows);
+}
+
+
+TEST(Image, shorten) {
+  Image img;
+  EXPECT_EQ(120, img.shorten().rows);
 }
