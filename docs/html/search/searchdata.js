de7581d77cae9e0e59c40e22e673cc67f4820c8c
Naveen Mangla <nmangla@umd.edu>
2022-10-19 20:34:12 -0400

    Doxygen

--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,27 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "cdfghimrs",
-  1: "cr",
-  2: "cdfgim",
-  3: "cdfs",
-  4: "h"
+  0: "h",
+  1: "h"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files",
-  2: "functions",
-  3: "variables",
-  4: "pages"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Files",
-  2: "Functions",
-  3: "Variables",
-  4: "Pages"
+  1: "Pages"
 };
 
