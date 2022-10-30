2fd7f40cabd0d57108e5d663fe7da24d2335824f
15abhinavgarg <15abhinavgarg@gmail.com>
2022-10-30 14:43:42 -0400

    Testcases changes due to error in coveralls

--- test/test.cpp
+++ test/test.cpp
@@ -16,30 +16,30 @@
 #include <yolo.hpp>
 
 TEST(Yolo_detect_Test,detect){
-  cv::Mat im = cv::imread("../videos/shiva.jpg",1);
+  cv::Mat im = cv::imread("../../videos/shiva.jpg",1);
   Image img(im);
-  Yolo yol("../models/yolov5n.onnx");
-  auto classes = yol.load_class_list("../segmentations/coco_names.txt");
+  Yolo yol("../../models/yolov5n.onnx");
+  auto classes = yol.load_class_list("../../segmentations/coco_names.txt");
 
   yol.detect(img,classes);
 }
 
 TEST(Yolo_constuctor_test,yolo){
   
-  Yolo yol("../models/yolov5n.onnx");
+  Yolo yol("../../models/yolov5n.onnx");
  
 }
 
 
 TEST(Image_constuctor_test,image){
-  cv::Mat im = cv::imread("../videos/shiva.jpg",1);
+  cv::Mat im = cv::imread("../../videos/shiva.jpg",1);
   Image img(im);
  
 }
 
 
 TEST(Image_sq_image_Test,sqare_image){
-  cv::Mat im = cv::imread("../videos/shiva.jpg",1);
+  cv::Mat im = cv::imread("../../videos/shiva.jpg",1);
   Image img(im);
   EXPECT_FLOAT_EQ(img.square_img().cols,img.square_img().rows);
 
@@ -48,8 +48,8 @@ TEST(Image_sq_image_Test,sqare_image){
 
 
 TEST(Image_draw_rectangles_Test,draw_rectangles){
-  Yolo yol("../models/yolov5n.onnx");
-  cv::Mat im = cv::imread("../videos/shiva.jpg",1);
+  Yolo yol("../../models/yolov5n.onnx");
+  cv::Mat im = cv::imread("../../videos/shiva.jpg",1);
   Image img(im);
   auto size = static_cast<int>(yol.output.size());
   
@@ -58,8 +58,8 @@ TEST(Image_draw_rectangles_Test,draw_rectangles){
 }
 
 TEST(Getting_rectangle_dimensions,getting_Rect_dim){
-  Yolo yol("../models/yolov5n.onnx");
-  cv::Mat im = cv::imread("../videos/shiva.jpg",1);
+  Yolo yol("../../models/yolov5n.onnx");
+  cv::Mat im = cv::imread("../../videos/shiva.jpg",1);
   Image img(im);
   std::vector<cv::Rect> boxes;
   float data[4] = {1,2,3,4};
